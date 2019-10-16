fe512ea1cf85b6f6cf3aef964ce84cc03bf73a33
Dmitrij Waskowski <dymitr@gmail.com>
2019-10-16 11:49:47 +0200

    first for io

---
+++ precache-manifest.2521548efba98cbbd831187c9492870d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "322865364f19cf3bfb5f",
+    "url": "/static/css/main.fb207af7.chunk.css"
+  },
+  {
+    "revision": "322865364f19cf3bfb5f",
+    "url": "/static/js/main.32286536.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "3e9275d9efabd7ed70cd59aedbc2e51e",
+    "url": "/index.html"
+  }
+];
