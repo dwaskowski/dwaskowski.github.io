f20e3b8cbe3d224f277cb27425ede8b005186488
Dmitrij Waskowski <Dmitrij.Waskowski@ecentria.com>
2022-05-30 13:08:27 -0500

    changes

---
+++ precache-manifest.3f4f061f8a0ecb9da9b8c0fdc3242dcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f561637149a41377cbe",
+    "url": "/static/css/main.0bc74603.chunk.css"
+  },
+  {
+    "revision": "3f561637149a41377cbe",
+    "url": "/static/js/main.eafcb6df.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0e4007777a51aae15166",
+    "url": "/static/js/2.cf2742ac.chunk.js"
+  },
+  {
+    "revision": "6a220519deef5fb60c31a52db41a38b5",
+    "url": "/index.html"
+  }
+];
